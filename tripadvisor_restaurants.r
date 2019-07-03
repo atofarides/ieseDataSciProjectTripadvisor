b169a661552c615642e95c69bf5f0e5060079e56
atofarides <antonist10@gmail.com>
2019-07-03 19:12:16 +0200

    Added scores to individual metrics

--- tripadvisor_restaurants.r
+++ tripadvisor_restaurants.r
@@ -10,6 +10,7 @@ library("readr")
 
 # Defining function to get info
 get_restaurant <- function(link){
+
   # Reading HTML
   html <- read_html(link)
   
@@ -58,29 +59,20 @@ get_restaurant <- function(link){
       }
     }
   
-  #Consildating everything in a tibble
-  restaurant <- data.frame(restaurant_name= restaurant_name,
-                           restaurant_rating = restaurant_rating,
-                           restaurant_number_reviews = restaurant_number_reviews,
-                           restaurant_rank = restaurant_rank,
-                           restaurant_postcode = restaurant_postcode,
-                           restaurant_price = restaurant_price,
-                           stringsAsFactors = FALSE)
-  return(restaurant)
-  
   # Get restaurant's scores (WIP)
   restaurant_score_nodes <- html_nodes(html, xpath = "//div[contains(@class,
                                        'restaurants-detail-overview-cards-RatingsOverviewCard__ratingQuestionRow')]") 
   restaurant_scores <- data.frame()
   if (length(restaurant_score_nodes) == 0){
+    restaurant_scores <- data.frame(metric=NA,score=NA)
   } else{
     index <- seq(from = 1, to = length(restaurant_score_nodes), by =1)
     
     for (i in index){
-      metric <- html_nodes(restaurant_score_nodes,xpath = "//span[contains(@class,'restaurants-detail-overview-cards-RatingsOverviewCard__ratingText')]")[i] %>%
+      metric <- html_nodes(html,xpath = "//span[contains(@class,'restaurants-detail-overview-cards-RatingsOverviewCard__ratingText')]")[i] %>%
         html_text()
-      score <-  html_nodes(restaurant_score_nodes,xpath = "//span[contains(@class,'ui_bubble_rating bubble_')]")[i] %>%
-        html_attr(name = "class") %>%
+      score <-  html_nodes(html,xpath = "//span[contains(@class,'restaurants-detail-overview-cards-RatingsOverviewCard__ratingBubbles')]/span")[i] %>%
+        html_attrs() %>%
         str_extract(pattern = "[0-9]{2}") %>%
         str_replace(pattern = "([0-9])([0-9])",replacement = "\\1.\\2" ) %>%
         as.numeric()
@@ -88,9 +80,27 @@ get_restaurant <- function(link){
       restaurant_scores <- bind_rows(restaurant_scores,new_row)
     }
   }
+  
+  #Consildating everything in a tibble
+  restaurant <- data.frame(restaurant_name= restaurant_name,
+                           restaurant_rating = restaurant_rating,
+                           restaurant_number_reviews = restaurant_number_reviews,
+                           restaurant_rank = restaurant_rank,
+                           restaurant_postcode = restaurant_postcode,
+                           restaurant_price = restaurant_price,
+                           restaurant_scores = restaurant_scores,
+                           stringsAsFactors = FALSE)
+  
+  # Tidying the restaurant scores column
+  restaurant <- spread(restaurant,key=restaurant_scores.metric,value=restaurant_scores.score,sep = "_") 
+  
+  #Removing the NA column if it exists
+  if(sum(str_detect(names(restaurant),"NA")) != 0){
+    restaurant <- select(restaurant,-'restaurant_scores.metric_NA')
+  }
+  
+  return(restaurant)
 }
-
-
 
 # load RDS file 
 restaurant_links <- readRDS(file.path("~","GitHub","ieseDataSciProjectTripadvisor","restaurant_links.rds"))
